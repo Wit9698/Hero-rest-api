401c4d436773a4dcc3dba77a67cb8344a5e0c6f4
Witold <gasowski.witold@gmail.com>
2022-02-22 16:13:46 +0100

    add some random heroes by javafaker

--- src/main/java/pl/coderslab/hero/HeroApplication.java
+++ src/main/java/pl/coderslab/hero/HeroApplication.java
@@ -1,7 +1,14 @@
 package pl.coderslab.hero;
 
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.coderslab.hero.model.Hero;
+import pl.coderslab.hero.repository.HeroRepository;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class HeroApplication {
@@ -10,4 +17,9 @@ public class HeroApplication {
         SpringApplication.run(HeroApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner(HeroRepository heroRepository){
+        Faker faker = new Faker();
+        return args -> IntStream.range(0,100).forEach(i ->heroRepository.save(new Hero(faker.superhero().name())));
+    }
 }
