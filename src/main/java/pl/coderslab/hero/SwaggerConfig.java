b9aef4c431c3461d8561337306a0be7836d05508
Witold <gasowski.witold@gmail.com>
2022-02-22 19:43:13 +0100

    pageable with jhipster

--- src/main/java/pl/coderslab/hero/SwaggerConfig.java
+++ src/main/java/pl/coderslab/hero/SwaggerConfig.java
@@ -1,11 +1,14 @@
 package pl.coderslab.hero;
 
 
+import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.coderslab.hero.util.PageableParameterBuilderPlugin;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.TypeNameExtractor;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -30,5 +33,9 @@ public class SwaggerConfig {
                 .apiInfo(apiInfo()).select()
                 .apis((RequestHandlerSelectors.basePackage("pl.coderslab.hero")))
                 .build();
+    }
+    @Bean
+    PageableParameterBuilderPlugin pageableParameterBuilderPlugin (TypeNameExtractor nameExtractor, TypeResolver resolver){
+        return new PageableParameterBuilderPlugin(nameExtractor, resolver);
     }
 }
